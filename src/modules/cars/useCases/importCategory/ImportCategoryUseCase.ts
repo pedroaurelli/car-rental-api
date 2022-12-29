c371ff89231ae0dc750a366b97ba9a4e5eb9b82e
pedroaurelli <pedroaureliano.contato@gmail.com>
2022-12-29 04:00:01 -0300

    Add fs.unlink method to remove file inside temp folder

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -28,6 +28,7 @@ export default class ImportCategoryUseCase {
         })
       })
         .on('end', () => {
+          fs.promises.unlink(file.path)
           resolve(categories)
         })
         .on('error', (err) => {
